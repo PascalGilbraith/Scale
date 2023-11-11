83de409adf57000662f839bee9caf5fa684bb4c5
Pascal Gilbraith <pascal.gilbraith@gmail.com>
2023-11-11 14:01:53 +0000

    Add button animation

--- button.cs
+++ button.cs
@@ -7,6 +7,7 @@ public partial class button : Area2D
 	public delegate void ButtonPushedEventHandler();
 
 	public bool IsPushed { get; private set; } = false;
+	public bool IsSingleUse { get; set; } = true;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -16,18 +17,26 @@ public partial class button : Area2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		// Since we don't loop the animation we need to return to idle once it's done
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		if (animatedSprite2D.Animation == "press" && animatedSprite2D.FrameProgress == 1)
+		{
+			animatedSprite2D.Play("idle");
+		}
 	}
 
 	private void _on_body_entered(Node2D body)
 	{
-		// TODO: Animate the button.
-		if (!IsPushed)
+		// I'm only interested in collisions with the character (and maybe boxes in the future)
+		if (body is player)
 		{
-			// I'm only interested in collisions with the character (and maybe boxes in the future)
-			if (body is player)
+			var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+			animatedSprite2D.Play("press");
+
+			IsPushed = true;
+			if (!IsSingleUse || !IsPushed)
 			{
-				IsPushed = true;
-				EmitSignal(SignalName.ButtonPushed);
+			EmitSignal(SignalName.ButtonPushed);
 			}
 		}
 	}
