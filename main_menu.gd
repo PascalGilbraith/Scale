91e43f7458349aa32473724ece22a05c801d8ce0
Pascal Gilbraith <pascal.gilbraith@gmail.com>
2023-11-19 21:29:46 +0000

    Clean up main_menu script

--- main_menu.gd
+++ main_menu.gd
@@ -3,14 +3,6 @@ class_name main_menu_gd
 
 @export var first_scene: PackedScene
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _on_button_play_pressed():
 	get_tree().change_scene_to_packed(first_scene)
 
