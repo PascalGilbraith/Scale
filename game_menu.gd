5f7d485748ddbc4a51b569b17da20f11b9ce8e97
Pascal Gilbraith <pascal.gilbraith@gmail.com>
2023-11-19 21:29:23 +0000

    Convert game_menu to GDScript

---
+++ game_menu.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+class_name game_menu
+
+func _on_button_play_pressed():
+	var scene_file = get_tree().current_scene.scene_file_path
+	if scene_file.ends_with("main.tscn"):
+		get_tree().reload_current_scene()
+		hide()
+		get_tree().paused = false
+	else:
+		get_tree().change_scene_to_file("res://main.tscn")
+		hide()
+		get_tree().paused = false
+
+func _on_button_quit_pressed():
+	get_tree().quit()
+
+func _on_button_resume_pressed():
+	# Resume current game
+	hide()
+	get_tree().paused = false
